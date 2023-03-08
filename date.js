7c27d63714b2c004a32e8e6b0d0a8a3f74dcf7ed
Michael Villamor <michael.villamor300@gmail.com>
2023-03-08 16:30:30 -0500

    fixed bug with wrong format for 'day'

--- date.js
+++ date.js
@@ -1,36 +1,31 @@
 let currentDate = new Date();
 let months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-]
-
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 let month = Number(currentDate.getMonth()) + 1;
-if (month < 10)
-month = `0${month}`
+if (month < 10) month = `0${month}`;
 
-const day = currentDate.getUTCDate();
+let day = currentDate.getUTCDate();
+if (day < 10) day = `0${day}`
 
-
-const formattedDate = `2022-${month}-${day}`
-
+const formattedDate = `2022-${month}-${day}`;
 
 const year = currentDate.getFullYear();
 
+const currentMonth = months[currentDate.getMonth()];
 
-const currentMonth = months[currentDate.getMonth()]
+const dateToday = `${currentMonth} ${day} ${year}`;
 
-const dateToday = `${currentMonth} ${day} ${year}`
-
-
-export {currentDate, formattedDate, month, day, dateToday}
+export { currentDate, formattedDate, month, day, dateToday };
